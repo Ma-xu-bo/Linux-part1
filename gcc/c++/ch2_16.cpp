d9e69bdc9e6b4108a4ebf70a95d8995f85ab56d7
me <me@example.com>
2020-08-25 22:25:30 +0800

    Add linked_Stack

---
+++ gcc/c++/ch2_16.cpp
@@ -0,0 +1,25 @@
+#include<linked_Stack.h>
+int main()
+{
+  linked_Stack<int>s;
+  s.ins_linked_Stack(50);
+  s.ins_linked_Stack(60);
+  s.ins_linked_Stack(70);
+  s.ins_linked_Stack(80);
+  s.ins_linked_Stack(90);
+  s.ins_linked_Stack(100);
+  cout<<"输出栈中的元素："<<endl;
+  s.prt_linked_Stack();
+  if (s.flag_linked_Stack())
+    cout<<"栈顶元素："<<s.read_linked_Stack()<<endl;
+  if (s.flag_linked_Stack())
+    cout<<"退栈元素："<<s.del_linked_Stack()<<endl;
+  if (s.flag_linked_Stack())
+    cout<<"退栈元素："<<s.del_linked_Stack()<<endl;
+  if (s.flag_linked_Stack())
+    cout<<"退栈元素："<<s.del_linked_Stack()<<endl;
+  cout<<"再次输出栈中的元素："<<endl;
+  s.prt_linked_Stack();
+  return 0;
+    
+}
---
+++ gcc/c++/include/linked_Stack.h
@@ -0,0 +1,75 @@
+//linked_Stack.h
+#include<iostream>
+using namespace std;
+template<class T>
+struct node
+{
+  T d;
+  node *next;
+};
+template<class T>
+class linked_Stack
+{
+ private:
+  node<T> *top;
+ public:
+  linked_Stack();
+  void prt_linked_Stack();
+  int flag_linked_Stack();
+  void ins_linked_Stack(T);
+  T del_linked_Stack();
+  T read_linked_Stack();
+};
+template<class T>
+linked_Stack<T>::linked_Stack()
+{
+  top=NULL;
+  return;
+}
+template<class T>
+void linked_Stack<T>::prt_linked_Stack()
+{
+  node<T> *p;
+  p=top;
+  if (p==NULL) { cout<<"空栈！"<<endl; return;}
+  do { cout<<p->d<<endl;
+    p=p->next;}
+  while (p!=NULL);//biao ji
+  return;
+}
+template<class T>
+int linked_Stack<T>::flag_linked_Stack()
+{
+  if (top==0) return(0);
+  return (1);
+}
+
+template<class T>
+void linked_Stack<T>::ins_linked_Stack(T x)
+{
+  node<T> *p;
+  p = new node<T>;
+  p->d=x;
+  p->next=top;
+  top=p;
+  return;
+}
+
+template<class T>
+T linked_Stack<T>::del_linked_Stack()
+{
+  T y;
+  node<T> *q;
+  if (top==NULL) { cout<<"空栈！"<<endl; return (0);}
+  q=top;
+  y=q->d;
+  top=q->next;
+  delete q;
+  return(y);
+    }
+template<class T>
+T linked_Stack<T>::read_linked_Stack()
+{
+  if (top==NULL) { cout<<"空栈！"<<endl; return(0);}
+  return(top->d);
+}
