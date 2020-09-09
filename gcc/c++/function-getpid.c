6fd280b88b5cb3dc00b9eef284e619cbae4465b0
me <me@example.com>
2020-09-09 22:19:24 +0800

    add function-getpid

---
+++ gcc/c++/function-getpid.c
@@ -0,0 +1,11 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+int main(){
+  pid_t pid,ppid;
+  pid = getpid();
+  ppid = getppid();
+  printf("当前进程的ID号为：%d\n",pid );
+  printf("当前进程的父进程ID号为：%d\n",ppid );
+  return 0;
+}
