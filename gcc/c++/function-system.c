9eca3b6229eb9a8e11d7ea5bba645afafd311e89
me <me@example.com>
2020-09-10 13:56:10 +0800

    add fork,system

---
+++ gcc/c++/function-system.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int ret;
+  printf("系统的进程号是：%d\n",getpid());
+  ret  = system("ping www.baidu.com -c 2");
+  printf("返回值为：%d\n,ret");
+  return 0;
+}
